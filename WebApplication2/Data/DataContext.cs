b2efc7c532dde9d9b971332d0cb5ca3d52606a7b
infq <diazagueromario@gmail.com>
2022-02-06 07:28:32 -0300

    Add project files.

---
+++ WebApplication2/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+
+        {
+        }
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Character> Characters { get; set; }
+
+    }
+}
